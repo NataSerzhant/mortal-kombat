6673157deafd61e86af08d3fe12848282a757430
NataSerzhant <74452195+NataSerzhant@users.noreply.github.com>
2021-03-19 12:36:09 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+﻿//когда все элементы страницы уже будут загружены начать..
+$(document).ready(function() {
+    //подключить к нашему блоку app
+    $('#app').vide({
+    mp4: 'video/video.mp4',
+    webm: 'video/video.webm',
+    ogv: 'video/video.ogv',
+    poster: 'img/cover-image.svg'
+    });
+});
